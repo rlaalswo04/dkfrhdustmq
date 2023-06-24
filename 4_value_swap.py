0116804329637ec8c0ce86a851a0f50d7da18d16
unknown <bravegirls1234@gmail.com>
2023-06-24 11:57:18 +0900

    값교환

---
+++ 4_value_swap.py
@@ -0,0 +1,27 @@
+def value_swap(x, y):
+    temp = x
+    x = y 
+    y = temp
+    return x, y
+
+x = 10
+y = 20
+# 현재 출력 10, 20
+print(f'x: {x}, y: {y}')
+
+# 기대값 x=20 y=10
+# 정석
+
+# temp = x
+# x = y
+# y = temp 
+# print(x)
+# print(y)
+
+# 트릭
+# x,y = y,x 
+
+res1,res2 = value_swap(x, y)
+print(f'x: {res1}, y:{res2}')
+
+
